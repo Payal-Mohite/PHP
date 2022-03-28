356275ce7407f0367e8d149a51512e4c071cb92f
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-03-28 23:10:51 +0530

    Add files via upload

---
+++ 02_Logical_Operators.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<body>
+<?php
+	
+	$a=2;
+	$b=3;
+	if($a==2 && $b==3)
+	
+		echo "<br>True";
+	
+	if($a==2 and $b==3)
+	
+		echo "<br>True";
+		
+	if($a==2 or $b==4)
+	
+		echo "<br>True";
+	
+	if($a==2 || $b==3)
+	
+		echo "<br>True";
+	
+	if($a==2 Xor $b==3)
+	
+		echo "<br>True";
+	
+	if($a!==4)
+	
+		echo "<br>True";
+?>
+</body>
+</html>
