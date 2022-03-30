0c62a01ee2c338db063eac1e7ff2c883b99327a3
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-03-30 22:26:59 +0530

    Add files via upload

---
+++ 04_Calender_switch.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<body>
+<?php
+$day="10";
+
+switch($day){
+	case 1:
+		echo"Monday";
+		break;
+	case 2:
+		echo "tuesday";
+		break;
+	case 3:
+		echo"Wednesday";
+		break;
+	case 4:
+		echo"Thursday";
+		break;
+	case 5:
+		echo"Friday";
+		break;
+	case 6:
+		echo"saturday";
+		break;
+	case 7:
+		echo"Sunday";
+		break;
+	default:
+		echo"Invalid number";
+		break;
+		
+}
+?>
+
+</body>
+</html>
