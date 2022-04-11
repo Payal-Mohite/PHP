726e4daa0723839961fa2399e8df351d8e6658d7
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-04-11 15:27:11 +0530

    Add files via upload

---
+++ 10_Multidiamentional_Array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+	$marks = array (
+	  array("Payal",250,78),
+	  array("Ashu",260,73),
+	  array("Sakshi",275,80),
+
+	);
+	  
+	echo $marks[0][0].$marks[0][1].$marks[0][2]."<br>";
+	echo $marks[1][0].$marks[1][1].$marks[1][2]."<br>";
+	echo $marks[2][0]. $marks[2][1].$marks[2][2]."<br>";
+	echo $marks[3][0].$marks[3][1].$marks[3][2]."<br>";
+	?>
+
+</body>
+</html>
